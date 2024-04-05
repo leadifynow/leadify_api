3a1a885d1dc0c41102fbfd6af8420063c0dc2828
Alex <m.alejandro.quintero@gmail.com>
2024-04-05 14:36:42 -0600

    test

--- src/main/java/com/api/leadify/dao/InterestedDao.java
+++ src/main/java/com/api/leadify/dao/InterestedDao.java
@@ -46,6 +46,7 @@ public class InterestedDao {
     public void createInterested(Interested interested) {
 
         try {
+            System.out.println(interested);
             // comentario de prueba
             // Sacamos los valores que necesitamos para validaciones, etc
             UUID workspaceId = interested.getWorkspace();
